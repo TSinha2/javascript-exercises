9c1b91e3becd5c975e7813d5c09371ea5bad2865
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-29 15:02:54 +0530

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+	ans = ((fahrenheit -32) * (5/9))
+	return Math.round(ans * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+	ans = ( (celsius* (9/5)) + 32)
+	return Math.round(ans * 10) / 10;
 };
 
 // Do not edit below this line
