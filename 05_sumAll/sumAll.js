bc404b9ce38cc93cb68422f340859f4e62bc512c
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-29 14:44:08 +0530

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	sum = 0;
+	if (start < -1 || end < -1)
+	{
+		return "ERROR";
+	}
+	if (!(typeof start === "number" && typeof end === "number"))
+	{
+		return "ERROR";
+	}
+	if (end > start)
+	{
+		first = start;
+		last = end;
+	}
+	else
+	{
+		first = end;
+		last = start;
+	}
+
+	for (let i = first; i < last; i++)
+	{
+		sum += i;
+	}
+	return sum+last;
 
 };
 
