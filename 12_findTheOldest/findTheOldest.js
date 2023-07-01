fab572bb46f89dbcc0574d45cef1b6adc34e76b9
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-01 13:42:27 +0530

    Solve ex.12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,33 @@
-const findTheOldest = function() {
+const age = function(person)
+{
+	let currentYear = Number(Date().split(" ")[3]);
+	if (person.yearOfDeath === undefined)
+	{
+		return currentYear-person.yearOfBirth;
+	}
+	else
+	{
+		return person.yearOfDeath - person.yearOfBirth;
+	}
+}
+
+const findTheOldest = function(people) {
+
+	oldestPerson = people[0];
+	oldestAge = age(people[0]);
+	for (let i = 0; i < people.length; i++)
+	{
+		console.log("NAME: " + people[i].name); 
+		console.log("AGE: " + age(people[i]));
+		if (age(people[i]) > oldestAge)
+		{
+			oldestPerson = people[i];	
+			oldestAge = age(people[i]);
+		}
+	}
+	return oldestPerson;
+
+	
 
 };
 
