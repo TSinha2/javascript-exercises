7f96c11141d4efdc0a5aa1d42b5bc3e35fbf7f34
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-01 12:44:31 +0530

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b;
+
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a-b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+	let sum = 0;
+	arr.forEach(element => sum += element);
+	return sum
 	
 };
 
-const multiply = function() {
+const multiply = function(...nums) {
+	let ans = 1;
+	nums.forEach(element => ans *= element);
+	return ans;
 
 };
 
-const power = function() {
+const power = function(a, b) {
+	return Math.pow(a, b);
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+	if (a == 0 || a == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return a * factorial(a-1);
+	}
 	
 };
 
