68f8bc2544ed2a59db62a1b254d11598299780a5
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-01 13:04:34 +0530

    Solve ex. 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+	// Make string all lowercase
+	inputStr = inputStr.toLowerCase();
+
+	// Remove punctuation
+	inputStr = inputStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+
+	// Remove all spaces
+	toCheck = '';
+	inputStr.split(" ").forEach(element => toCheck += element);
+	console.log("Filtered String: " + toCheck);
+
+	for (let i = 0; i < toCheck.length; i++)
+	{
+		if (toCheck[i] != toCheck[toCheck.length -1 -i])
+		{
+			return false;
+		}
+	}
+	return true; 
+
+
+	
 
 };
 
