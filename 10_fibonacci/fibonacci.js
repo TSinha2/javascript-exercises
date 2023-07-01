becc48a32f4d6432d256242a2f5ec4c28ee0198f
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-01 13:17:51 +0530

    Solve ex.10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	// Make sure type is number 
+	num = Number(num);
+	if (num < 0)
+	{
+		return "OOPS";
+	}
+
+	fib_nums = [1, 1];
+	if (num < 3)
+	{
+		return fib_nums[num -1];
+	}
+	while (fib_nums.length != num)
+	{
+		fib_nums.push(fib_nums[fib_nums.length -1] + fib_nums[fib_nums.length -2]);
+	}
+	console.log(fib_nums)
+	return fib_nums[num -1];
+
 
 };
 
